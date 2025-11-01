69f7e8611b1ef147df4e6b1fb3419477c08256b6
4ntho0ny <anthonyhildebrandchendrik@gmail.com>
2025-11-01 12:31:42 -0400

    Jogo rodando com os três niveis de perguntas, validação de reposta errada, validação se ganhou o jogo, agora realizar mais testes antes de começar a criar as opções de ajuda.

--- show_do_milhao.c
+++ show_do_milhao.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
 #define QTD_PERGUNTAS_TOTAL 70
 #define QTD_PERGUNTAS_NIVEIS_1_2_3 20
 #define QTD_PERGUNTAS_NIVEL_4 10
 
 typedef enum {
-    ALT_A = 'A',
-    ALT_B = 'B',
-    ALT_C = 'C',
-    ALT_D = 'D',
-    ALT_1 = '1',
-    ALT_2 = '2',
-    ALT_3 = '3',
-    ALT_4 = '4',
-    ALT_5 = '5',
-} Alternativas; // PAREI AQUI
+    INPUT_A = 'A',
+    INPUT_B = 'B',
+    INPUT_C = 'C',
+    INPUT_D = 'D',
+    INPUT_1 = '1',
+    INPUT_2 = '2',
+    INPUT_3 = '3',
+    INPUT_4 = '4',
+    INPUT_5 = '5',
+} INPUT_USUARIO;
 
 typedef struct {
     char nivel;
@@ -30,88 +31,133 @@ void carregar_perguntas_por_nivel(pergunta *todas_perguntas, pergunta *perguntas
 int sortear_pergunta(int qtd_perguntas);
 int verificar_posicao_existe(pergunta *perguntas, int posicao_pergunta, int qtd_perguntas);
 void imprimir_pergunta(pergunta pergunta);
+int entrada_invalida(char entrada);
+void eliminar_pergunta(pergunta *perguntas, int posicao_pergunta, int qtd_perguntas);
+void msg_derrota();
+void msg_vitoria();
 
 int main() {
     FILE *file;
     
-    pergunta todas_perguntas[QTD_PERGUNTAS_TOTAL],
-    *perguntas_por_nivel;
+    pergunta todas_perguntas[QTD_PERGUNTAS_TOTAL];
+    pergunta pergunta_atual;
+    pergunta *perguntas_por_nivel;
 
-    int nvl_atual = 1, 
-    running = 1, 
-    qtd_perguntas_repondidas = 0,
-    qtd_perguntas_nvl = QTD_PERGUNTAS_NIVEIS_1_2_3,
-    pergunta_sorteada;
+    int nvl_atual = 1; 
+    int running = 1; 
+    int qtd_perguntas_repondidas = 0;
+    int qtd_perguntas_nvl = QTD_PERGUNTAS_NIVEIS_1_2_3;
+    int qtd_respostas_nvl = 5;
+    int pergunta_sorteada;
+    int nao_sortear_pergunta = 0;
 
-    float val_inicial = 0.0, 
-    val_pergunta = 0.0, 
-    val_acumulado = 0.0;
+    float val_pergunta = 1000.0;
+    float val_acumulado = 0.0;
 
     char entrada;
 
     carregar_perguntas(todas_perguntas);
 
-    // for (int i = 0; i < QTD_PERGUNTAS; i++) {
-    //     printf("\n[%d] Pergunta\n", i + 1);
-    //     printf("Nivel: %c\n", perguntas[i].nivel);
-    //     printf("Descrição: %s\n", perguntas[i].descricao);
-    //     printf("Alternativas:\n");   
-    //     for (int j = 0; j < 4; j++) {
-    //         // Imprime A), B), C) ou D)
-    //         printf(" %c) %s\n", 'A' + j, perguntas[i].alt[j]);
-    //     }  
-    //     printf("Alternativa correta: %c\n", perguntas[i].alt_correta);
-    //     printf("------------------------------------------\n");
-    // }
-
     while (running)
     {
-
         // Verificar o nível do jogador para alocar a quantidade de memória correta para armazenar as perguntas
         if (nvl_atual == 4) {
             qtd_perguntas_nvl = QTD_PERGUNTAS_NIVEL_4;
+            qtd_respostas_nvl = 1;
+            
+            pergunta *temp_ptr = (pergunta *)realloc(perguntas_por_nivel, qtd_perguntas_nvl * sizeof(pergunta));
+
+            if (temp_ptr == NULL)
+                break;
+
+            perguntas_por_nivel = temp_ptr;
+            free(temp_ptr);
         }
-        perguntas_por_nivel = malloc(qtd_perguntas_nvl * sizeof(pergunta));
+        else {
+            qtd_perguntas_nvl = QTD_PERGUNTAS_NIVEIS_1_2_3;
+            perguntas_por_nivel = (pergunta *)malloc(qtd_perguntas_nvl * sizeof(pergunta));
+        }
         
         // Carregar somentes as perguntas do nível atual do jogador
         carregar_perguntas_por_nivel(todas_perguntas, perguntas_por_nivel, nvl_atual);
 
-        while (1)
+        while (qtd_perguntas_repondidas < qtd_respostas_nvl)
         {
-            pergunta_sorteada = sortear_pergunta(qtd_perguntas_nvl);
+            if (nao_sortear_pergunta == 0) {
+                pergunta_sorteada = sortear_pergunta(qtd_perguntas_nvl);
+                pergunta_atual = perguntas_por_nivel[pergunta_sorteada];
+            }
+
+            printf("== ------------\n");
+            printf("== Pergunta %d - Nivel %d\n\n", qtd_perguntas_repondidas + 1, nvl_atual);            
+            printf("== Voce possui um total acumulado de R$ %.2f ==\n\n", val_acumulado);
+
+            imprimir_pergunta(pergunta_atual);
             
-            printf("==------------\n");
-            printf("== Pergunta %d", qtd_perguntas_repondidas + 1);            
-            printf("== Voce possui um total acumulado de R$ %f ==", val_acumulado);
-
-            imprimir_pergunta(perguntas_por_nivel[pergunta_sorteada]);
-
             // imprimir opções de ajuda
 
-            printf("~~~~~~~~~~~~~~~");
+            printf("~~~~~~~~~~~~~~~\n\n");
 
+            printf("Digite sua resposta: ");
+            scanf(" %c", &entrada);
+            
+            entrada = toupper(entrada);
+
+            if ((nao_sortear_pergunta = entrada_invalida(entrada))) {
+                
+                printf("\nXXXXXXXXX Entrada Invalida XXXXXXXXX\n\n");               
+                continue;
+            }
+
+            // if (entrada != toupper(pergunta_atual.alt_correta)) {
+            if (entrada != toupper(pergunta_atual.alt_correta)) {
+                msg_derrota();
+                running = 0;
+                break;
+            }
+            
+            printf("\n********** Certa Resposta! **********\n\n");
+
+            // Validar ajuda
+
+            eliminar_pergunta(perguntas_por_nivel, pergunta_sorteada, qtd_perguntas_nvl);
+ 
+            val_acumulado += val_pergunta;
+            qtd_perguntas_repondidas ++;
+            qtd_perguntas_nvl --;
+        }
+
+        if (running == 0)
+            break;
+
+        if (nvl_atual == 1) {
+            val_acumulado = 10000.00;
+            val_pergunta = 10000.00;
+        }
+        else if (nvl_atual == 2) {
+            val_acumulado = 100000.00;
+            val_pergunta = 100000.00;
+        }
+        else if (nvl_atual == 4) {
+            msg_vitoria();
             break;
         }
 
-
-        // for (int i = 0; i < 20; i++) {
-        //     printf("\n[%d] Pergunta\n", i + 1);
-        //     printf("Nivel: %c\n", perguntas_por_nivel[i].nivel);
-        //     printf("Descrição: %s\n", perguntas_por_nivel[i].descricao);
-        //     printf("Alternativas:\n");            
-        //     for (int j = 0; j < 4; j++) {
-        //         // Imprime A), B), C) ou D)
-        //         printf(" %c) %s\n", 'A' + j, perguntas_por_nivel[i].alt[j]);
-        //     }            
-        //     printf("Alternativa correta: %c\n", perguntas_por_nivel[i].alt_correta);
-        //     printf("------------------------------------------\n");
-        // }
-        running = 0;
+        nvl_atual ++; 
+        val_pergunta *= 10.00;
+        qtd_perguntas_repondidas = 0;
+        free(perguntas_por_nivel);
+        perguntas_por_nivel = NULL;
     }
 
-    
     return 0;
 }
+
+/*
+=================================================================
+=========================== FUNCTIONS ===========================
+=================================================================
+*/
 
 void carregar_perguntas(pergunta *perguntas) {
 
@@ -156,4 +202,34 @@ void imprimir_pergunta(pergunta pergunta) {
         for (int j = 0; j < 4; j++) {
             printf(" %c) %s\n", 'a' + j, pergunta.alt[j]);
         }            
+}
+
+int entrada_invalida(char entrada) {
+    if (entrada != INPUT_A && entrada != INPUT_B && entrada != INPUT_C && 
+        entrada != INPUT_D && entrada != INPUT_1 && entrada != INPUT_2 && 
+        entrada != INPUT_3 && entrada != INPUT_4 && entrada != INPUT_5)
+        return 1;
+    
+    return 0;
+}
+
+void eliminar_pergunta(pergunta *perguntas, int posicao_pergunta, int qtd_perguntas) {
+    for (int i = posicao_pergunta; i < qtd_perguntas; i++)
+    {
+        perguntas[i] = perguntas[i + 1];
+    }
+}
+
+void msg_derrota() {
+    printf("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n");
+    printf("\nXXXXXXXXX VOCE PERDEU O JOGO :( XXXXXXXXX\n");
+    printf("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n");
+}
+
+void msg_vitoria() {
+    printf("\n*****************************************\n");
+    printf("\n********** PARABENS, VOCE VENCEU! *******\n");
+    printf("\n*****************************************\n");
+    printf("\n********** VOCE É UM MILIONARIO! ********\n");
+    printf("\n*****************************************\n\n");
 }
